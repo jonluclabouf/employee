66baa2c979db912641da441aee579fa18872a16a
jonluclabouf <jonlucsp@gmail.com>
2023-08-01 17:56:43 -0400

    Trying to get POST working

--- demo/src/main/java/com/example/demo/OrganicDogApplication.java
+++ demo/src/main/java/com/example/demo/OrganicDogApplication.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class OrganicDogApplication {
+// @SpringBootApplication
+// public class OrganicDogApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrganicDogApplication.class, args);
-    }
+//     public static void main(String[] args) {
+//         SpringApplication.run(OrganicDogApplication.class, args);
+//     }
 
-}
+// }
